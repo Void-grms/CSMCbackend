caf8a8b8e66ddd7f3c2187756f2b2397bf0cb083
Void-grms <patrickislabardales@gmail.com>
2026-03-30 17:20:54 -0500

    Fix: Adicion de cors en server

--- src/api/server.js
+++ src/api/server.js
@@ -9,6 +9,7 @@ const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
 const { Pool } = require('pg');
+const cors = require('cors');
 
 const {
   getDashboard,
@@ -57,13 +58,10 @@ app.use('/api', (req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(204);
-  next();
-});
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   const t0 = Date.now();
